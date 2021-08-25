ddf17f7228810c07f10e9db22fb5693b128a5997
Dream-991029 <981846339@qq.com>
2021-08-25 15:25:49 +0800

    '创建项目'

---
+++ datamall/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for datamall project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'datamall.settings')
+
+application = get_asgi_application()
